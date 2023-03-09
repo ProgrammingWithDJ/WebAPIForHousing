86033119f7cdefe01f1f3f53ab663df2d9b4d241
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2023-03-09 19:16:48 +0530

    Add project files.

---
+++ Controllers/CityController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIForHousing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "Mumbai" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Pune";
+        }
+    }
+}
