6758e4b8c6d76c926ecb7e6739674da03050bce7
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2023-03-09 19:34:17 +0530

    Added DbContext class

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIForHousing.Models;
+
+namespace WebAPIForHousing.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<City> Cities { get; set; }
+    }
+}
