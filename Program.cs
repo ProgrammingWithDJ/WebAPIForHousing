80b0e01517b30a2ab21092be7b45b2ee8a68bf7c
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2023-03-09 20:37:40 +0530

    Added code to do the migrations and fetched the data from the SQL Azure database

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using WebAPIForHousing.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer());
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
