ff01d49f54de3800a82542cbff3e01ea152dfd1d
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2023-03-10 19:56:59 +0530

    Implemented the Repository Pattern

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using WebAPIForHousing.Data;
+using WebAPIForHousing.Data.Repo;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+builder.Services.AddScoped<ICityRepository, CityRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
