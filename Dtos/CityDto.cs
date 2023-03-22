f7bb281b2ec7e4c45fee1fc99fbaf22e6a8c7d12
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2023-03-22 22:50:08 +0530

    Added few validators

--- Dtos/CityDto.cs
+++ Dtos/CityDto.cs
@@ -1,11 +1,16 @@
-﻿namespace WebAPIForHousing.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIForHousing.Dtos
 {
     public class CityDto
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage ="Name is mandatory field")]
+        [StringLength(10, MinimumLength =2)]
         public string Name { get; set; }
 
+        [Required]
         public string Country{ get; set; }
     }
 }
--- Models/City.cs
+++ Models/City.cs
@@ -1,11 +1,15 @@
-﻿namespace WebAPIForHousing.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIForHousing.Models
 {
     public class City
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Country { get; set; }
 
         public DateTime LastUpdatedOn { get; set; }
