7f17900ad5a64a6a0e5263c7395ab26fe034a54b
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2023-03-23 22:53:25 +0530

    Added an Extenstion Method to the Middle ware pipeline to handle the exceeption

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using WebAPIForHousing.Helpers;
 using System.Net;
 using Microsoft.AspNetCore.Diagnostics;
 using Azure;
+using WebAPIForHousing.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,33 +25,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
-{
-    app.UseExceptionHandler(
-        options =>
-        {
-            options.Run(
-                async context =>
-                {
-                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                    var ex = context.Features.Get<IExceptionHandlerFeature>();
 
-                    if (ex != null)
-                    {
-                        await context.Response.WriteAsync(ex.Error.Message);
-                    }
-                }
-
-                );
-        }
-        
-        );
-}
+app.ConfigureExceptionHandler(app.Environment);
 
 app.UseHttpsRedirection();
 
